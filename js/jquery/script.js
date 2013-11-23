fc464cf23115e98f2015d88ff6aae7742ef2c970
Irene <irene.iaccio@gmail.com>
2013-11-23 23:55:49 +0100

    javascript

---
+++ js/jquery/script.js
@@ -0,0 +1,22 @@
+/* GIOCATTOLI ITALIANI */
+
+jQuery.noConflict();
+jQuery(document).ready(function(){
+	//effetti vetrina prodotti 
+    jQuery('.item').mouseover(function() {
+	    jQuery(this).find('.products-grid-info').hide();
+		jQuery(this).find('.actions-hover').show();
+    });
+	jQuery('.item').mouseleave(function() {
+		jQuery(this).find('.actions-hover').hide();
+		jQuery(this).find('.products-grid-info').show();
+    }); 
+	//top menù
+	jQuery('.padre').click(function() {
+	    jQuery(this).find('.sub-blocco-menu').fadeIn();	
+    });
+	jQuery('.padre').mouseleave(function() {
+	    jQuery(this).find('.sub-blocco-menu').hide();	
+    });
+});
+
